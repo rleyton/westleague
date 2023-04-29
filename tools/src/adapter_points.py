e75ef49d8960f89e172c81d1c04e3971ee89e660
Richard Leyton <richard@leyton.org>
2023-04-29 15:51:45 +0100

    use agecat results

--- tools/src/adapter_points.py
+++ tools/src/adapter_points.py
@@ -167,7 +167,7 @@ def calculate_competition_points(results, teams, event):
             reference[ageCat][gender]["penalty"] = penaltyPoints
             reference[ageCat][gender]["participants"] = totalParticipants
             reference[ageCat][gender]["counters"] = maxCounters
-            reference[ageCat][gender]["clubparticipation"] = results.groupby('clubnumber').count()['position'].to_dict()
+            reference[ageCat][gender]["clubparticipation"] = ageCatResults.groupby('clubnumber').count()['position'].to_dict()
             
             # Work through all the teams we know about
             for team in teams.index:
